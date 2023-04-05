8bbbaa4d450825849da42e33377bf4c115acc57f
asidikfauzi <asidikfauzi@gmail.com>
2023-04-05 18:57:34 +0700

    model familylist

--- models/familyList.go
+++ models/familyList.go
@@ -1,1 +1,9 @@
 package models
+
+type FamilyList struct {
+	ID         int    `gorm:"primaryKey;AUTO_INCREMENT;" json:"fl_id"`
+	CustomerId int    `gorm:"not null" json:"cst_id"`
+	Relation   string `gorm:"type:varchar(50);not null;" json:"fl_relation"`
+	Name       string `gorm:"type:varchar(50);not null;" json:"fl_name"`
+	DOB        string `gorm:"type:varchar(50);not null;" json:"fl_name"`
+}
