7b5d0013c962b045b5ef9748493315771fffc64e
asidikfauzi <asidikfauzi@gmail.com>
2023-04-05 18:58:08 +0700

    nationality

--- models/nationality.go
+++ models/nationality.go
@@ -1,1 +1,7 @@
 package models
+
+type Nationality struct {
+	ID   int    `gorm:"primaryKey;AUTO_INCREMENT;" json:"nationality_id"`
+	Name string `gorm:"type:varchar(50);not null" json:"nationality_name"`
+	Code string `gorm:"type:char(2);not null;" json:"nationality_code"`
+}
