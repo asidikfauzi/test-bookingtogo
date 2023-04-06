7b66d19fe0647a07aa3ae0848dda7379991d69b1
asidikfauzi <asidikfauzi@gmail.com>
2023-04-06 14:06:24 +0700

    utils

--- lib/utils/env.go
+++ lib/utils/env.go
@@ -1,1 +1,16 @@
 package utils
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+)
+
+func GetEnv(key string) string {
+	var appConfig map[string]string
+	appConfig, err := godotenv.Read()
+
+	if err != nil {
+		fmt.Println("Error reading .env file")
+	}
+	return appConfig[key]
+}
