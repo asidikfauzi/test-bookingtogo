9187390de430f6e2a84dc3b21ebdc377220375e7
asidikfauzi <asidikfauzi@gmail.com>
2023-04-06 14:08:58 +0700

    create database nationality insert

--- lib/database/nationality.go
+++ lib/database/nationality.go
@@ -1,1 +1,15 @@
 package database
+
+import (
+	"test-bookingtogo/config"
+	"test-bookingtogo/models"
+)
+
+func InsertNationality(postBody models.NationalityPost) (interface{}, error) {
+	nationality := models.Nationalities{
+		NationalityName: postBody.NationalityName,
+		NationalityCode: postBody.NationalityCode,
+	}
+	err := config.DB.Create(&nationality).Error
+	return nationality, err
+}
