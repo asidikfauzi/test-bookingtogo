3bc4bd61efb4094ef6dfaa8052747e321bb89152
asidikfauzi <asidikfauzi@gmail.com>
2023-04-07 00:15:27 +0700

    models response

--- models/response.go
+++ models/response.go
@@ -5,3 +5,19 @@ type ErrorResponse struct {
 	Message interface{} `json:"message"`
 	Status  string      `json:"status"`
 }
+
+type Response struct {
+	Code    int         `json:"code"`
+	Message string      `json:"message"`
+	Data    interface{} `json:"data"`
+}
+
+type ResponseGetList struct {
+	Code       int         `json:"code"`
+	Message    string      `json:"message"`
+	Data       interface{} `json:"data"`
+	Page       int         `json:"page"`
+	Limit      int         `json:"limit"`
+	TotalPage  int         `json:"totalPage"`
+	TotalCount int64       `json:"totalCount"`
+}
