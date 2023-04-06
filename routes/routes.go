2a0497b950c8de9f00d4a1fb2faf351155cb4a5a
asidikfauzi <asidikfauzi@gmail.com>
2023-04-07 00:15:46 +0700

    update routes

--- routes/routes.go
+++ routes/routes.go
@@ -10,8 +10,9 @@ import (
 
 func NewRouter() {
 	r := mux.NewRouter()
-	r.HandleFunc("/", controllers.GetNationalities).Methods("GET")
-	r.HandleFunc("/", controllers.CreateNationality).Methods("POST")
+	r.HandleFunc("/nationality", controllers.GetNationalities).Methods("GET")
+	r.HandleFunc("/nationality/{id}", controllers.GetNationalityById).Methods("GET")
+	r.HandleFunc("/nationality", controllers.CreateNationality).Methods("POST")
 
 	// start server
 	fmt.Println("RUN", utils.GetEnv("PORT"))
