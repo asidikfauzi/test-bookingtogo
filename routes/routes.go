0ce25fee09f951c5a9b3b835d2701f581d9dab8d
asidikfauzi <asidikfauzi@gmail.com>
2023-04-07 02:19:27 +0700

    group route nationality

--- routes/routes.go
+++ routes/routes.go
@@ -10,11 +10,15 @@ import (
 
 func NewRouter() {
 	r := mux.NewRouter()
-	r.HandleFunc("/nationality", controllers.GetNationalities).Methods("GET")
-	r.HandleFunc("/nationality/{id}", controllers.GetNationalityById).Methods("GET")
-	r.HandleFunc("/nationality", controllers.CreateNationality).Methods("POST")
-	r.HandleFunc("/nationality/{id}", controllers.UpdateNationality).Methods("PUT")
-	r.HandleFunc("/nationality/{id}", controllers.DeleteNationality).Methods("DELETE")
+
+	//--------start nationality group router
+	nationalityRouter := r.PathPrefix("/nationality").Subrouter()
+	nationalityRouter.HandleFunc("", controllers.GetNationalities).Methods("GET")
+	nationalityRouter.HandleFunc("/{id}", controllers.GetNationalityById).Methods("GET")
+	nationalityRouter.HandleFunc("", controllers.CreateNationality).Methods("POST")
+	nationalityRouter.HandleFunc("/{id}", controllers.UpdateNationality).Methods("PUT")
+	nationalityRouter.HandleFunc("/{id}", controllers.DeleteNationality).Methods("DELETE")
+	//--------end nationality group router
 
 	// start server
 	fmt.Println("RUN", utils.GetEnv("PORT"))
