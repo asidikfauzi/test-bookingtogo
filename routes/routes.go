b8faa539dafd9bbd00ab0d08493f99ac51842000
asidikfauzi <asidikfauzi@gmail.com>
2023-04-06 14:08:10 +0700

    routes

--- routes/routes.go
+++ routes/routes.go
@@ -1,1 +1,19 @@
 package routes
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"net/http"
+	"test-bookingtogo/controllers"
+	"test-bookingtogo/lib/utils"
+)
+
+func NewRouter() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", controllers.GetNationalities).Methods("GET")
+	r.HandleFunc("/", controllers.CreateNationality).Methods("POST")
+
+	// start server
+	fmt.Println("RUN", utils.GetEnv("PORT"))
+	http.ListenAndServe(":"+utils.GetEnv("PORT"), r)
+}
