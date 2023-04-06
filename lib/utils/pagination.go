c2c1e9249f888b7b6871e752c90beb9d09cb546c
asidikfauzi <asidikfauzi@gmail.com>
2023-04-07 00:15:01 +0700

    utils pagination

--- lib/utils/pagination.go
+++ lib/utils/pagination.go
@@ -1,1 +1,17 @@
 package utils
+
+import "strconv"
+
+func GetPagination(pageStr, limitStr string) (int, int, int) {
+	page, err := strconv.Atoi(pageStr)
+	if err != nil {
+		page = 1 // set a default value if the conversion fails
+	}
+
+	limit, err := strconv.Atoi(limitStr)
+	if err != nil {
+		limit = 10 // set a default value if the conversion fails
+	}
+	offset := limit * (page - 1)
+	return offset, limit, page
+}
