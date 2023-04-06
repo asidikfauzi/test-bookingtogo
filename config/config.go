3c6b18bffa9adaa66f825acb54c72ab2679090a4
asidikfauzi <asidikfauzi@gmail.com>
2023-04-06 14:05:59 +0700

    config

--- config/config.go
+++ config/config.go
@@ -2,48 +2,37 @@ package config
 
 import (
 	"fmt"
-	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
+	"test-bookingtogo/lib/utils"
 	"test-bookingtogo/models"
 )
 
 var DB *gorm.DB
 
 func InitDB() {
-	var appConfig map[string]string
-	appConfig, err := godotenv.Read()
-
-	if err != nil {
-		fmt.Println("Error reading .env file")
-	}
 
 	postgresCredentials := fmt.Sprintf(
 		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
-		appConfig["DB_HOST"],
-		appConfig["DB_USER"],
-		appConfig["DB_PASSWORD"],
-		appConfig["DB_NAME"],
-		appConfig["DB_PORT"],
+		utils.GetEnv("DB_HOST"),
+		utils.GetEnv("DB_USER"),
+		utils.GetEnv("DB_PASSWORD"),
+		utils.GetEnv("DB_NAME"),
+		utils.GetEnv("DB_PORT"),
 	)
-
-	DB, err = gorm.Open(postgres.New(postgres.Config{
+	DB, _ = gorm.Open(postgres.New(postgres.Config{
 		DSN: postgresCredentials,
 	}), &gorm.Config{})
-
-	if err != nil {
-		panic(err)
-	}
-
-	InitMigrate()
+	fmt.Println("Server Success")
+	//InitMigrate()
 }
 
 func InitMigrate() {
-	DB.AutoMigrate(&models.Nationalitys{})
+	DB.AutoMigrate(&models.Nationalities{})
 	DB.AutoMigrate(&models.Customers{})
 	DB.AutoMigrate(&models.FamilyLists{})
 
-	DB.Where("1 = 1").Delete(&models.Nationalitys{})
+	DB.Where("1 = 1").Delete(&models.Nationalities{})
 	DB.Where("1 = 1").Delete(&models.Customers{})
 	DB.Where("1 = 1").Delete(&models.FamilyLists{})
 }
---
+++ controllers/nationalityController.go
@@ -0,0 +1,15 @@
+package controllers
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func GetNationalitys(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("MASUK")
+	return
+}
+
+func CreateNationality(w http.ResponseWriter, r *http.Request) {
+
+}
