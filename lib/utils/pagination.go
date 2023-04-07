08664e1a0b86ef5ead2debc2835744559916ecd2
asidikfauzi <asidikfauzi@gmail.com>
2023-04-08 05:58:33 +0700

    fix pagination

--- lib/utils/pagination.go
+++ lib/utils/pagination.go
@@ -5,12 +5,12 @@ import "strconv"
 func GetPagination(pageStr, limitStr string) (int, int, int) {
 	page, err := strconv.Atoi(pageStr)
 	if err != nil {
-		page = 1 // set a default value if the conversion fails
+		page = 1
 	}
 
 	limit, err := strconv.Atoi(limitStr)
 	if err != nil {
-		limit = 10 // set a default value if the conversion fails
+		limit = 100
 	}
 	offset := limit * (page - 1)
 	return offset, limit, page
