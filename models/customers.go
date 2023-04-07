0586418c9153217292db6916e92b6af48ee87733
asidikfauzi <asidikfauzi@gmail.com>
2023-04-08 05:58:51 +0700

    fix models customer

--- models/customers.go
+++ models/customers.go
@@ -7,18 +7,19 @@ type Customers struct {
 	NationalityId int           `gorm:"not null" json:"nationality_id"`
 	CstName       string        `gorm:"type:char(50);not null" json:"cst_name"`
 	CstDOB        time.Time     `gorm:"not null;" json:"cst_dob"`
-	CstPhoneNum   string        `gorm:"type:varchar(20);not null" json:"cst_phoneNum"`
+	CstPhoneNum   string        `gorm:"type:varchar(20);not null" json:"cst_phone_num"`
 	CstEmail      string        `gorm:"type:varchar(50);" json:"cst_email"`
-	Nationalities Nationalities `gorm:"foreignKey:NationalityId;references:NationalityId"`
+	Nationalities Nationalities `gorm:"foreignKey:NationalityId;references:NationalityId;" json:"nationality"`
 }
 
 type CustomersResponse struct {
-	CstId         int       `json:"cst_id"`
-	NationalityId int       `json:"nationality_id"`
-	CstName       string    `json:"cst_name"`
-	CstDOB        time.Time `json:"cst_dob"`
-	CstPhoneNum   string    `json:"cst_phone_num"`
-	CstEmail      string    `json:"cst_email"`
+	CstId           int    `json:"cst_id"`
+	NationalityId   int    `json:"nationality_id"`
+	NationalityName string `json:"nationality_name"`
+	CstName         string `json:"cst_name"`
+	CstDOB          string `json:"cst_dob"`
+	CstPhoneNum     string `json:"cst_phone_num"`
+	CstEmail        string `json:"cst_email"`
 }
 
 type CustomerPost struct {
