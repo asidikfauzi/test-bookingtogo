47edfd03f9ca00c5a64b545075fece6fe2c0f2ff
asidikfauzi <asidikfauzi@gmail.com>
2023-04-07 14:32:29 +0700

    models customers

--- models/customers.go
+++ models/customers.go
@@ -5,8 +5,25 @@ import "time"
 type Customers struct {
 	CstId         int           `gorm:"primaryKey;AUTO_INCREMENT" json:"cst_id"`
 	NationalityId int           `gorm:"not null" json:"nationality_id"`
+	CstName       string        `gorm:"type:char(50);not null" json:"cst_name"`
 	CstDOB        time.Time     `gorm:"not null;" json:"cst_dob"`
 	CstPhoneNum   string        `gorm:"type:varchar(20);not null" json:"cst_phoneNum"`
 	CstEmail      string        `gorm:"type:varchar(50);" json:"cst_email"`
 	Nationalities Nationalities `gorm:"foreignKey:NationalityId;references:NationalityId"`
+}
+
+type CustomerPost struct {
+	NationalityId int    `json:"nationality_id" form:"nationality_id" validate:"required"`
+	CstName       string `json:"cst_name" form:"cst_name" validate:"required"`
+	CstDOB        string `json:"cst_dob" form:"cst_dob" validate:"required"`
+	CstPhoneNum   string `json:"cst_phone_num" form:"cst_phone_num" validate:"required"`
+	CstEmail      string `json:"cst_email" form:"cst_email" validate:"required"`
+}
+
+type CustomerUpdate struct {
+	NationalityId int    `json:"nationality_id" form:"nationality_id" validate:"required"`
+	CstName       string `json:"cst_name" form:"cst_name" validate:"required"`
+	CstDOB        string `json:"cst_dob" form:"cst_dob" validate:"required"`
+	CstPhoneNum   string `json:"cst_phone_num" form:"cst_phone_num" validate:"required"`
+	CstEmail      string `json:"cst_email" form:"cst_email" validate:"required"`
 }
